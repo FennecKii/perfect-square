737135f95d87cdd0154cc7d68f48e81ef082e211
Andrew Liu <andrewliu101720@gmail.com>
2025-04-06 17:40:59 -0600

    Godot boiler plate

---
+++ whiteboard.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var drawing: bool = false
+var point_spacing: float = 100
+var point_position: PackedVector2Array = []
+var point_check: PackedVector2Array = []
+
+func _input(event):
+	if event is InputEventMouseButton:
+		drawing = event.is_pressed()
+		point_position.append(event.position)
+		point_check.append(event.position)
+		queue_redraw()
+	
+	if event is InputEventMouseMotion and drawing:
+		if vec_len(event.position.x - point_check[len(point_check) - 1].x, event.position.y - point_check[len(point_check) - 1].y) >= point_spacing:
+			point_check.append(event.position)
+		point_position.append(event.position)
+		queue_redraw()
+	
+	if not drawing:
+		point_position = []
+		point_check = []
+		queue_redraw()
+
+func _draw():
+	if len(point_position) < 1:
+		return
+	elif len(point_position) == 1:
+		draw_circle(point_position[0], 10, Color.BLACK)
+	else:
+		draw_polyline(point_position, Color.BLACK, 10, true)
+
+func vec_len(x: float, y: float):
+	return sqrt(x**2 + y**2)
