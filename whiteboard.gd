2441da6586e689ad628c7faf010da7ed4514f45f
Jesse <83730324+dev-jesse@users.noreply.github.com>
2025-04-06 20:17:53 -0400

    Math

--- whiteboard.gd
+++ whiteboard.gd
@@ -1,21 +1,22 @@
 extends Node2D
 
 var drawing: bool = false
-var point_spacing: float = 100
+var point_spacing: float = 20
 var point_position: PackedVector2Array = []
 var point_check: PackedVector2Array = []
 
 func _input(event):
+	var curr_position = event.position - (get_viewport_rect().size / 2)
 	if event is InputEventMouseButton:
 		drawing = event.is_pressed()
-		point_position.append(event.position)
-		point_check.append(event.position)
+		point_position.append(curr_position)
+		point_check.append(curr_position)
 		queue_redraw()
 	
 	if event is InputEventMouseMotion and drawing:
-		if vec_len(event.position.x - point_check[len(point_check) - 1].x, event.position.y - point_check[len(point_check) - 1].y) >= point_spacing:
-			point_check.append(event.position)
-		point_position.append(event.position)
+		if vec_len(curr_position.x - point_check[len(point_check) - 1].x, curr_position.y - point_check[len(point_check) - 1].y) >= point_spacing:
+			point_check.append(curr_position)
+		point_position.append(curr_position)
 		queue_redraw()
 	
 	if not drawing:
@@ -26,10 +27,11 @@ func _input(event):
 func _draw():
 	if len(point_position) < 1:
 		return
-	elif len(point_position) == 1:
+		
+	if len(point_position) == 1:
 		draw_circle(point_position[0], 10, Color.BLACK)
 	else:
 		draw_polyline(point_position, Color.BLACK, 10, true)
 
-func vec_len(x: float, y: float):
+func vec_len(x: float, y: float) -> float:
 	return sqrt(x**2 + y**2)
