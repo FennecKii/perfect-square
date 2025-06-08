470e8a52b9829b494d821a2a0280964e0ef887bd
Andrew Liu <andrewliu101720@gmail.com>
2025-06-08 17:04:25 -0600

    Audio system implementation

---
+++ audio_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@onready var background_music: AudioStreamPlayer = $"Background Music"
+
+func play_background_music(music_track: AudioStream, volume: float = 0.0, pitch: float = 0.0) -> void:
+	background_music.stream = music_track
+	background_music.volume_db = volume
+	background_music.pitch_scale = pitch
+	background_music.stream.loop_mode = AudioStreamWAV.LoopMode.LOOP_FORWARD
+	background_music.stream.loop_begin = 0 * music_track.sound.mix_rate
+	background_music.stream.loop_end = music_track.sound.get_length() * music_track.sound.mix_rate
+	background_music.play()
+
+func play_sfx(sfx_stream: AudioStream, volume: float = 0.0, pitch: float = 0.0):
+	var new_sfx = AudioStreamPlayer2D.new()
+	new_sfx.stream = sfx_stream
+	new_sfx.volume_db = volume
+	new_sfx.pitch_scale = pitch
+	new_sfx.bus = "SFX"
+	add_child(new_sfx)
+	new_sfx.play()
+	new_sfx.finished.connect(new_sfx.queue_free)
